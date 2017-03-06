cc7af34dddabd74dcd4f8ca533fba1aa9c4719d1
Josh Cox <josh@webhosting.coop>
2017-03-05 23:21:51 -0600

    domain

--- assets/js/themeswitcher.js
+++ assets/js/themeswitcher.js
@@ -2,7 +2,7 @@
 
 var style_cookie_name = "style" ;
 var style_cookie_duration = 30 ;
-var style_domain = "joshuacox.github.io" ;
+var style_domain = "gaianysus.com" ;
 
 // *** END OF CUSTOMISABLE SECTION ***
 // You do not need to customise anything below this line
